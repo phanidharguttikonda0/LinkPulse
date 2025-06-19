31d1f430390bf85c6b7fbe6a286a350c93ffb74c
phanidharguttikonda0 <phanidharguttikonda0@gmail.com>
2025-06-19 10:11:21 +0530

    tests for validating user requests

--- link-pulse-app/tests/unit_testing/authentication_test.go
+++ link-pulse-app/tests/unit_testing/authentication_test.go
@@ -1,8 +1,10 @@
 package unit_testing
 
 import (
+	"errors"
 	"fmt"
 	"github.com/phanidharguttikonda0/LinkPulse/middlewares"
+	"regexp"
 	"testing"
 )
 
@@ -16,4 +18,64 @@ func TestAuthorizationCheck(t *testing.T) {
 		t.Errorf("Error verifying token: %v", err)
 	}
 	fmt.Println(claims)
+}
+
+// Define your regex patterns (example patterns, adjust as needed)
+var (
+	UsernameRegex = regexp.MustCompile(`^[a-zA-Z0-9_]{3,16}$`)                             // Example: 3-16 alphanumeric or underscore
+	MobileRegex   = regexp.MustCompile(`^[0-9]{10}$`)                                      // Example: 10 digits
+	EmailRegex    = regexp.MustCompile(`^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$`) // Standard email
+)
+
+// User struct for sign-in
+type User struct {
+	Username string
+	Password string
+}
+
+// NewUser struct for sign-up
+type NewUser struct {
+	Username string
+	Password string
+	Mobile   string
+	MailId   string
+}
+
+// isValid checks if a string matches a given regex
+func isValid(regex *regexp.Regexp, s string) bool {
+	return regex.MatchString(s)
+}
+
+// SignInValidation validates user sign-in credentials
+func (signIn *User) SignInValidation() (bool, error) {
+	if isValid(UsernameRegex, signIn.Username) {
+		if len(signIn.Password) >= 8 {
+			return true, nil
+		} else {
+			return false, errors.New("password should be at least 8 characters")
+		}
+	} else {
+		return false, errors.New("username is invalid")
+	}
+}
+
+// SignUpValidation validates new user sign-up credentials
+func (signUp *NewUser) SignUpValidation() (bool, error) {
+	if isValid(UsernameRegex, signUp.Username) {
+		if len(signUp.Password) >= 8 {
+			if isValid(MobileRegex, signUp.Mobile) {
+				if isValid(EmailRegex, signUp.MailId) {
+					return true, nil
+				} else {
+					return false, errors.New("invalid mail id")
+				}
+			} else {
+				return false, errors.New("invalid mobile number")
+			}
+		} else {
+			return false, errors.New("password should be at least 8 characters")
+		}
+	} else {
+		return false, errors.New("username is invalid")
+	}
 }
