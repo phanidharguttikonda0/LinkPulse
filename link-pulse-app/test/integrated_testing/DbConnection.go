27043dbd1050d057e13d3586e9d398b154bdf9c9
phanidharguttikonda0 <phanidharguttikonda0@gmail.com>
2025-06-22 11:15:04 +0530

    added more logs for easy debugging

--- link-pulse-app/test/integrated_testing/DbConnection.go
+++ link-pulse-app/test/integrated_testing/DbConnection.go
@@ -12,7 +12,7 @@ func DbConnection() (*sql.DB, error) {
 	user := os.Getenv("POSTGRES_USER")
 	password := os.Getenv("POSTGRES_PASSWORD")
 	dbname := os.Getenv("POSTGRES_DB")
-
+	log.Printf("Connecting to database with user: %s and password: %s and dbname: %s", user, password, dbname)
 	dsn := fmt.Sprintf("host=localhost port=5432 user=%s password=%s dbname=%s sslmode=disable",
 		user, password, dbname)
 
