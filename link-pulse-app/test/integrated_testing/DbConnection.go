e6f549e5555b423a6d3a9df9fcd3a179ef1b019e
phanidharguttikonda0 <phanidharguttikonda0@gmail.com>
2025-06-22 11:07:12 +0530

    updated the env variables that we are getting

--- link-pulse-app/test/integrated_testing/DbConnection.go
+++ link-pulse-app/test/integrated_testing/DbConnection.go
@@ -8,14 +8,12 @@ import (
 )
 
 func DbConnection() (*sql.DB, error) {
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASS")
-	dbname := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
+	user := os.Getenv("POSTGRES_USER")
+	password := os.Getenv("POSTGRES_PASSWORD")
+	dbname := os.Getenv("POSTGRES_DB")
 
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		host, port, user, password, dbname)
+	dsn := fmt.Sprintf("host=localhost port=5432 user=%s password=%s dbname=%s sslmode=disable",
+		user, password, dbname)
 
 	log.Println("The DSN was : ", dsn)
 
